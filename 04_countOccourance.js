90728789adc6dc6ae72900fb1cc0c1561b8aad35
VIVEK ANAND <47303600+imvivekanand@users.noreply.github.com>
2023-10-09 15:18:32 +0530

    count occourances

--- 04_countOccourance.js
+++ 04_countOccourance.js
@@ -1,13 +1,25 @@
-function countOccourances(str, char){
-    let letters = str.split("");
-    let count = 0;
+// function countOccourances(str, char){
+//     let count = 0;
     
-    for(let i=0; i < letters.length; i++){
-        if(letters[i] === char){
-            count += 1;
-        }
-    }
-    return(count);
+//     for(let i=0; i < str.length; i++){
+//         if(str[i] === char){
+//             count ++;
+//         }
+//     }
+//     console.log(count);
+// }
+
+function countOccourances(str, char){
+    let letters = str.split(char).length - 1;
+    console.log(letters);
+    // let count = 0;
+    
+    // for(let i=0; i < letters.length; i++){
+    //     if(letters[i] === char){
+    //         count += 1;
+    //     }
+    // }
+    // console.log(count);
 }
 
-countOccourances("vivevVk", "v")
+countOccourances("vivek", "v")
