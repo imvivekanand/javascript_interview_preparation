6833b244700cb1d6f1efd24c606a892c745b722a
VIVEK ANAND <47303600+imvivekanand@users.noreply.github.com>
2023-10-10 17:15:50 +0530

    reverse a string

--- 08_ReverseString.js
+++ 08_ReverseString.js
@@ -1,7 +1,27 @@
 const reverseString = (str) => {
-    for(let i=0; i<str.length(); i++){
-        console.log(str[i])
-    }
-}
 
-console.log(reverseString("Vivek"));
+    //SOl-1
+    // return str.split("").reverse().join("");
+    //reverse function reverse an array. We cannot reverse the string.
+
+    //SOL-2
+    // let revArray = [];
+
+    // for(let i=str.length - 1; i >= 0; i--){
+    //     revArray.push(str[i])
+    // }
+    // console.log(revArray.join(""));
+
+    //SOL-3
+    let revStr = "";
+
+    for(let i=str.length - 1; i >= 0; i--){
+        revStr += str[i];
+    }
+    console.log(revStr);
+
+
+
+
+}
+reverseString("Vivek");
