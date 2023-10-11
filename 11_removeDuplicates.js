19eb4c1bd58cc43d60c43866abb75cba4c6a90b8
VIVEK ANAND <47303600+imvivekanand@users.noreply.github.com>
2023-10-11 11:43:49 +0530

    remove duplicate element using sets

--- 11_removeDuplicates.js
+++ 11_removeDuplicates.js
@@ -1,12 +1,19 @@
 const removeDuplicates = arr => {
-    const uniqueArr = [];
 
-    for(let i=0; i< arr.length; i++){
-        if(!uniqueArr.includes(arr[i])){
-            uniqueArr.push(arr[i])
-        }
-    }
-    console.log(uniqueArr);
+    //Sol - 1
+
+    // const uniqueArr = [];
+
+    // for(let i=0; i< arr.length; i++){
+    //     if(!uniqueArr.includes(arr[i])){
+    //         uniqueArr.push(arr[i])
+    //     }
+    // }
+    // console.log(uniqueArr);
+
+    //Sol - 2
+
+    console.log(new Set(arr));
 }
 
 removeDuplicates([1, 1, 4, 6, 9, 9, true, true, false, true, "vivek", "anand", "vivek"])
