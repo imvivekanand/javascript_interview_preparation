631ec54463daa4b77000d677a5d1b3989263bef1
VIVEK ANAND <47303600+imvivekanand@users.noreply.github.com>
2023-10-11 11:11:45 +0530

    palindrome

---
+++ 09_Palindrome.js
@@ -0,0 +1,13 @@
+//A word, phrase, or sequence that reads the same backwards as forwards, e.g. madam or nurses run.
+
+//Sol - 1:
+
+// function isPalindrome(str){
+//     const formattedString = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+//     let revString = formattedString.split("").reverse().join("");
+//     console.log(formattedString === revString);
+// }
+
+// isPalindrome("viv")
+
+//Sol -2
